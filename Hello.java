14a75237b7e7e41980b09e6b960875e9e6e310ff
ajwatson20 <55361630+ajwatson20@users.noreply.github.com>
2019-09-15 15:33:38 -0400

    Update Hello.java
    Day-3 Work

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 class Hello
 {
-  public static void main(String[] args);
+  public static void main(String[] args)
   {
     System.out.println("Hello, World");
+    System.out.println("I Rule");
   }
   }
